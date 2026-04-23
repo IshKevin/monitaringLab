44d3030508d7dad872f6427cc650a23b14eff0c5
IshKevin <carterk279@gmail.com>
2026-04-23 22:29:12 +0200

    Add S3 bucket policy for CloudTrail logging

---
+++ infra/modules/bucket/policy.tf
@@ -0,0 +1,32 @@
+resource "aws_s3_bucket_policy" "cloudtrail" {
+  bucket = aws_s3_bucket.this.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid = "AWSCloudTrailAclCheck"
+        Effect = "Allow"
+        Principal = {
+          Service = "cloudtrail.amazonaws.com"
+        }
+        Action   = "s3:GetBucketAcl"
+        Resource = aws_s3_bucket.this.arn
+      },
+      {
+        Sid = "AWSCloudTrailWrite"
+        Effect = "Allow"
+        Principal = {
+          Service = "cloudtrail.amazonaws.com"
+        }
+        Action   = "s3:PutObject"
+        Resource = "${aws_s3_bucket.this.arn}/AWSLogs/*"
+        Condition = {
+          StringEquals = {
+            "s3:x-amz-acl" = "bucket-owner-full-control"
+          }
+        }
+      }
+    ]
+  })
+}
